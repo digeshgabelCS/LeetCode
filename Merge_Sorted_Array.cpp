80bf05ac1c4d998f5306283e66493761558a940f
Digesh Gabel <digeshgabel786@gmail.com>
2025-06-27 23:23:14 +0530

    Add files via upload

---
+++ Merge_Sorted_Array.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // Start from the end of both arrays
+        int i = m - 1; // Last valid element in nums1
+        int j = n - 1; // Last element in nums2
+        int k = m + n - 1; // Last position in nums1
+
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Example input
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    int m = 3;
+    vector<int> nums2 = {2, 5, 6};
+    int n = 3;
+
+    // Call merge function
+    solution.merge(nums1, m, nums2, n);
+
+    // Output merged array
+    cout << "Merged array: ";
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
